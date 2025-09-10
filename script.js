839bf7f261262fe90d8919b3c46bda1de6597d6d
keusinis <220072052+keusinis@users.noreply.github.com>
2025-09-10 14:34:13 +0300

    Add game logic

--- script.js
+++ script.js
@@ -1,39 +1,70 @@
+const CHOICES = ["rock", "paper", "scissors"];
+
+const scoreState = {
+  human: 0,
+  computer: 0,
+};
+
 function getComputerChoice() {
-    let choices = ["rock", "paper", "scissors"];
-    return choices[Math.floor(Math.random() * 3)];
-}
-function getHumanChoice() {
-    return prompt("Enter your choice (rock, paper, scissors):");
-}
-function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
-    if(humanChoice === computerChoice)
-        return null;
-    switch(humanChoice) {
-        case "rock":
-            if(computerChoice === "scissors")
-                return true;
-            break;
-        case "paper":
-            if(computerChoice === "rock")
-                return true;
-            break;
-        case "scissors":
-            if(computerChoice === "paper")
-                return true;
-            break;
-        default:
-            return null;
-        }
-        return false;
+  return CHOICES[Math.floor(Math.random() * CHOICES.length)];
 }
 
-let humanScore = 0;
-let computerScore = 0;
-for(let i = 0; i < 5; i++) {
-    let outcome = playRound(getHumanChoice(), getComputerChoice());
-    if(outcome === true)
-        humanScore++;
-    if(outcome === false)
-        computerScore++;
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) return "tie";
+  if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    return "win";
+  }
+  return "loose";
 }
+
+function updateScore() {
+  document.querySelector("#human-score span").textContent = scoreState.human;
+  document.querySelector("#computer-score span").textContent = scoreState.computer;
+}
+
+function showRoundResult(result, human, computer) {
+  let message = {
+    empty: "",
+    win: "You win!",
+    loose: "You loose!",
+    tie: "It's a tie!",
+  };
+  document.querySelector("#human-choice span").textContent = human;
+  document.querySelector("#computer-choice span").textContent = computer;
+  document.querySelector(".outcome > p").textContent = message[result];
+}
+
+function checkGameOver() {
+  if (playUpTo === scoreState.computer || playUpTo === scoreState.human)
+    resetGame();
+}
+
+function resetGame() {
+  let winner =
+    scoreState.computer > scoreState.human
+      ? "Computer won the game"
+      : "You won the game";
+  alert(winner);
+  scoreState.computer = 0;
+  scoreState.human = 0;
+  updateScore();
+  showRoundResult("empty", "", "");
+}
+const playUpTo = parseInt(prompt("Play up to:", 5));
+document.querySelectorAll(".choices > button").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const human = btn.id;
+    const computer = getComputerChoice();
+    const result = playRound(btn.id, computer);
+    
+    if (result === "win") scoreState.human++;
+    if (result === "loose") scoreState.computer++;
+    updateScore();
+    showRoundResult(result, human, computer);
+    checkGameOver();
+  });
+});
